f2b31ed12ddf42730b13aa0555beedbd63137b23
franciscaserrao <francisca.s.serrao@gmail.com>
2024-03-20 11:27:06 +0000

    Created basic HTML

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import "./App.css";
 import React from "react";
+import Weather from "./Weather";
 
 export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <h1>Weather App</h1>
-
+        <Weather />
         <footer>
           This project was coded by{" "}
           <a href="https://github.com/franciscaserrao">Francisca Serrão</a>, is
