e2789a2ba8af22eb90a06117f9a8cad848992b92
franciscaserrao <francisca.s.serrao@gmail.com>
2024-03-20 10:44:27 +0000

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
 import "./App.css";
+import React from "react";
 
 export default function App() {
   return (
     <div className="App">
-      Weather App
-      <footer>
-        This project was coded by{" "}
-        <a href="https://github.com/franciscaserrao">Francisca Serrão</a>, is
-        open-sourced on{" "}
-        <a href="https://github.com/franciscaserrao/react-weather-final">
-          GitHub
-        </a>{" "}
-        and hosted on{" "}
-        <a href="https://master--nimble-biscuit-3ea091.netlify.app/">Netlify</a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+
+        <footer>
+          This project was coded by{" "}
+          <a href="https://github.com/franciscaserrao">Francisca Serrão</a>, is
+          open-sourced on{" "}
+          <a href="https://github.com/franciscaserrao/react-weather-final">
+            GitHub
+          </a>{" "}
+          and hosted on{" "}
+          <a href="https://master--nimble-biscuit-3ea091.netlify.app/">
+            Netlify
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
