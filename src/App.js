b9dacaeef142302d3e287c7ba785836bd390c9cf
franciscaserrao <francisca.s.serrao@gmail.com>
2024-03-20 10:08:38 +0000

    Updated footer

--- src/App.js
+++ src/App.js
@@ -4,7 +4,16 @@ export default function App() {
   return (
     <div className="App">
       Weather App
-      <h1>Hello</h1>
+      <footer>
+        This project was coded by{" "}
+        <a href="https://github.com/franciscaserrao">Francisca Serrão</a>, is
+        open-sourced on{" "}
+        <a href="https://github.com/franciscaserrao/react-weather-final">
+          GitHub
+        </a>{" "}
+        and hosted on{" "}
+        <a href="https://master--nimble-biscuit-3ea091.netlify.app/">Netlify</a>
+      </footer>
     </div>
   );
 }
